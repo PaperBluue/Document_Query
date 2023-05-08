61cf966c4a09d77380f7e8f20a9d4f8dd06f3cee
PaperBluue <PaperBluue>
2023-05-08 19:33:30 +0800

    写一半了突然有事，润了

--- document_query/run_example.py
+++ document_query/run_example.py
@@ -1,14 +1,15 @@
 # 导库
 import document_query
 
+# 如果不用度盘查询可以忽略段代码--------------------
 # 这俩玩意是用于获取access_token，在代码中用不到
 document_query.AppKey = ""
 document_query.url_get_access_token = ""
-
-# 如果不用度盘查询可以忽略
+# 必要的token
 document_query.access_token = ""
+# ____________________________________________
 
-# 查询的基本信息保存对象，如果有多个查询需求就可直接再例化一个
+# 查询的基本信息保存对象，如果有多个查询需求就可直接再创建一个
 datas_1 = document_query.init_datas(
     # 其中只有rc_path是必填项，其他都是可选项
     # 被查询的文件夹路径
@@ -19,13 +20,13 @@ datas_1 = document_query.init_datas(
     panbaidu_path=None,
     # 输出查询结果的文件路径
     existlist_path=None,
-    # 名单路径
+    # 查询名单路径
     rc_path=None
 )
 
-# 例化顶层对象
+# 创建顶层对象
 top = document_query.datass()
-# 把例化的基本信息保存对象全部添加导顶层模块中
+# 把创建的基本信息保存对象全部添加导顶层模块中
 top.data_append(datas_1)
 # 开始
 top.progress_all()
